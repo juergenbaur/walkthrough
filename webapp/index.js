0ac7c92fcfd2d9c364029277e6bb7419e076dee3
juergen baur <juergen.baur@avisco.de>
2020-09-29 13:29:43 +0000

    Step 3: Controls

--- webapp/index.js
+++ webapp/index.js
@@ -1,6 +1,11 @@
 sap.ui.define([
+	"sap/m/Text"
 
-], function () {
+], function (Text) {
 	"use strict";
-	alert("UI5 is ready");
+
+	new Text({
+		text: "Hello World"
+	}).placeAt("content");
+
 });
